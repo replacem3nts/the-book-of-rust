c874a4522d09b492a02a3161f279e38ae21b2b20
donny landis <donny.landis@gmail.com>
2023-04-11 01:25:50 -0400

    continues progressing --slowly-- on lifetimes

--- generics_ch/lifetimes_basics/src/main.rs
+++ generics_ch/lifetimes_basics/src/main.rs
@@ -27,4 +27,59 @@ fn main() {
 // They show some examples, but the sentence above pretty much captures it
 
 // <---------------- Generic Lifetimes in Functions  ---------------->
+fn main() {
+    let str1 = String::from("wxyz");
+    let str2 = "abc";
+
+    let result = longest(str1.as_str(), str2);
+    println!("The longest string is: {result}")
+}
+
+fn longest(x: &str, y: &str) -> &str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+// Above code will throw an error saying: expected return value to be a "named lifetime
+// parameter. help: does not say if returned borrowed value is x or y"
+
+// Since neither we, nor the borrow checker can determine the answer of which will be
+// returned, we add 'generic lifetime parameters' that define the relationship between
+// the references so borrow checker can analyze
+
+// <----------------- Lifetime Annotation Syntax  ----------------->
+// >> Lifetime annotations don't change how long lifetimes live. They describe the
+// relationships of multiple references to each other without affecting the lifetimes. <<
+
+// Syntax: always starts with an apostrophe ('); usually always lowercase and very short
+// comes after the & but before the reference type as seen below:
+
+// &i32        // a reference
+// &'a i32     // a reference with an explicit lifetime
+// &'a mut i32 // a mutable reference with an explicit lifetime
+
+// <----------------- Lifetime in Fn Signatures  ----------------->
+// Syntax similar to generic type parameters in fns: declared inside angle brackets
+// between fn name and param list as seen below:
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+//This notation signifies: "the returned reference will be valid as long as both params
+// are valid"
+
+// Lifetime annotations always go in the function signature, never in the function body.
+// Giving fn signatures lifetime contracts means Rust compiler analysis is simplified
+
+// In the above example, when the function receives concrete references, 'a will take on
+// the concrete lifetime which is the smaller of lifetimes x and y.
+
+// <--------------- Thinking in Terms of Lifetimes --------------->
 //
